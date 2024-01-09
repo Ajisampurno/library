d017f53f9e6c505f90099125eab5b21f5e263f26
aji sampurno <ajisampurno121@gmail.com>
2024-01-09 20:07:36 +0700

    create view author,book,catalog,publisher,member

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.author.index');
+    }
 }
--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.catalog.index');
+    }
 }
