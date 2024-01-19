4b9432d469f61076cbd753ba7cff0de26dfb59bb
aji sampurno <ajisampurno121@gmail.com>
2024-01-19 17:20:30 +0700

    create table halaman catalog

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
     public function index()
     {
-        return view('admin.catalog.index');
+        $catalogs = Catalog::with('books')->get();
+
+        return view('admin.catalog.index', compact('catalogs'));
     }
 }
